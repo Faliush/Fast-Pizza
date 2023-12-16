1c2620567f0fe852d49d7c503a7e3ca14fb94526
nickfaliush <faliushdev@gmail.com>
2023-12-16 17:58:17 +0200

    Set up a file structure and implemented routes

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import eslint from "vite-plugin-eslint";
-// npm i eslint vite-plugin-eslint eslint-config-react-app --save-dev
 
 export default defineConfig({
-  plugins: [react(), eslint()],
+  plugins: [react()],
 });
